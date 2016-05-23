547bbbd1f081a8324d7ac46b89b641e336b07f9f
Micaías Rubio <mrubiog@mr.local>
2016-05-22 21:02:52 -0400

    Initial Commit

---
+++ Prueba/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Prueba
+//
+//  Created by Micaías Rubio on 22-05-16.
+//  Copyright © 2016 Micaías Rubio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
